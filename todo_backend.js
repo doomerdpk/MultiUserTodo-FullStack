ebc7d674e6b3e2e8e154831dfaeafc9f47941899
doomerdpk <doomerdpk@gmail.com>
2024-09-11 22:58:33 +0530

    Removed Port

--- todo_backend.js
+++ todo_backend.js
@@ -284,6 +284,6 @@ app.delete("/delete-todo/:idx", authentication, async function (req, res) {
   }
 });
 
-app.listen(3000, function () {
-  console.log("Backend is running on http://localhost:3000");
-});
+//app.listen(3000, function () {
+//  console.log("Backend is running on http://localhost:3000");
+//});
